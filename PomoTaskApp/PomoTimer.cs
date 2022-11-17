bd68a1ba8d4f9f74ffd154b5d5731e64a3fe6d35
david ukpoju <ukpojuojdave12@gmail.com>
2022-11-17 11:50:07 +0100

    PomoTimeTaskApp

---
+++ PomoTaskApp/PomoTimer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomoTaskApp
+{
+    public class PomoTimer
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        private int workTim;
+        private int restTim;
+
+        public PomoTimer(int workTim, int restTim)
+        {
+           // _stopwatch = stopwatch;
+            this.workTim = workTim;
+            this.restTim = restTim;
+        }
+
+        
+
+        public void beginWork()
+        {
+            Stopwatch stopwatch = new Stopwatch();  
+
+
+            stopwatch.Start();
+            var workStartTime = DateTime.Now.ToString("HH:mm");
+
+            Thread.Sleep(workTim);
+
+            stopwatch.Stop();
+            var workEndTime = DateTime.Now.ToString("HH:mm");
+
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTim = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.TotalMilliseconds/10);
+
+
+            Console.WriteLine($"\n Your work lasted for {elapsedTim}  \n {workStartTime} - {workEndTime} ");
+
+        }
+
+        public void restTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var restStartTime = DateTime.Now.ToString("  HH:mm");
+
+            Thread.Sleep(restTim);
+
+            stopWatch.Stop();
+            var restEndTime = DateTime.Now.ToString("HH:mm");
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine($"\nRest time lasted for {elapsedTime} \n{restStartTime} - {restEndTime}");
+        }
+    }
+}
---
+++ PomoTaskApp/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace PomoTaskApp
+{
+    internal class Program
+    {
+
+        public static double ConvertMinutesToMilliseconds(int minutes)
+        {
+            return TimeSpan.FromMinutes(minutes).TotalMilliseconds;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to your Pomo task DashBoard");
+
+
+            while (true)
+            {
+               
+
+                
+                Console.WriteLine("Enter work time in minutes: ");
+                int workTime = Convert.ToInt32(Console.ReadLine());
+
+                
+
+
+                Console.WriteLine("\nEnter rest time in minutes: ");
+                int restTime = Convert.ToInt32(Console.ReadLine());
+                
+
+
+                
+                int convertWorkTimeToMs = (int)ConvertMinutesToMilliseconds(workTime);
+                int convertRestTimeToMs = (int)ConvertMinutesToMilliseconds(restTime);
+
+
+               
+                PomoTimer timer = new PomoTimer(convertWorkTimeToMs, convertRestTimeToMs);
+
+
+
+                
+
+                Console.WriteLine("\nDo you want to start?(y/n): ");
+
+                string? startTimer = Console.ReadLine();
+
+
+                if (startTimer == "y")
+                {
+                    Console.WriteLine("\nWork timer is running...");
+                    timer.beginWork();
+
+                    Console.WriteLine("\nRest timer is running...");
+                    timer.restTime();
+                }
+
+                else if (startTimer == "n")
+                {
+                    continue;
+                }
+
+                else if (startTimer != "n" && startTimer != "y")
+                {
+                    Console.WriteLine("\nPlease enter a valid input, y for yes and n for no");
+                    continue;
+                }
+
+
+               
+                Console.WriteLine("\nWould you like to continue (y/n): ");
+
+                string? continueUsingTimer = Console.ReadLine();
+
+                if (continueUsingTimer == "y")
+                {
+                    continue;
+                }
+                else if (continueUsingTimer == "n")
+                {
+                    break;
+                }
+                else if (continueUsingTimer != "n" && continueUsingTimer != "y")
+                {
+                    Console.WriteLine("\nPlease enter a valid input next time, y for yes and n for no");
+                    break;
+                }
+
+            }
+
+         
+           
+        }
+    }
+}
